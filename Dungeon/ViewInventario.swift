e3c726f5bc49079b975e5a339d3c7e860fd2bf47
auser <auser@a22o19.local>
2018-12-19 20:06:31 +0100

    Creating and linking all the game app views.

---
+++ Dungeon/ViewInventario.swift
@@ -0,0 +1,18 @@
+//
+//  ViewInventario.swift
+//  Dungeon
+//
+//  Created by auser on 19/12/2018.
+//  Copyright © 2018 DAM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewInventario: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
---
+++ Dungeon/VistaInventario.swift
@@ -0,0 +1,9 @@
+//
+//  VistaInventario.swift
+//  Dungeon
+//
+//  Created by auser on 19/12/2018.
+//  Copyright © 2018 DAM. All rights reserved.
+//
+
+import Foundation
